4afa79bb6f5b6b35736807be68846d460ca3e255
Ler Zhi Yao <storm_1986@hotmail.com>
2020-03-05 23:32:52 +0800

    Initial Commit

---
+++ Add 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Add 1
+//
+//  Created by Ler Zhi Yao on 05/03/2020.
+//  Copyright © 2020 Ler Zhi Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
